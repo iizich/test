b1648d37eae7abfbae02d651172a41b7ddc0e030
alex <leshkanchik1337@gmail.com>
2022-07-14 10:33:36 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -2,17 +2,16 @@
 #include <vector>
 #include <algorithm>
 
+
 int main()
 {
     std::vector<int> v;
-    v.push_back(123);
-    v.push_back(456);
-    v.push_back(789);
     v.push_back(101112);
     std::sort(v.begin(),v.end());
     for(auto&& item : v){
         std::cout << item << ' ';
     }
+    int max = 100;
     std::cout << std::endl;
     return 0;
 }
